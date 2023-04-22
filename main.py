3d3757379be0153191960b8bdb0f1b466e4f6123
trisha112 <tagra002@ucr.edu>
2023-04-22 16:23:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
+from Problem import Problem
+#from Node import Node
+#from Tree import Tree
+from UniformCostSearch import *
 def main():
+    initial = []
     print("Welcome to #25 8 puzzle solver.")
+    goalState = [1,2,3,4,5,6,7,8,9]
     puzzleChoice = input("Type “1” to use a default puzzle, or “2” to enter your own puzzle.\n")
-    if puzzleChoice == 1:
-        # default puzzle code
-        print("#1")
-    elif puzzleChoice == 2:
+    if puzzleChoice == "1":
+        defaultPuzzle = [1,0,3,4,2,6,7,5,8]
+        problemVal = Problem(defaultPuzzle, goalState)
+
+    if puzzleChoice == "2":
         print("Enter your puzzle, use a zero to represent the blank")
-        first = input(print("Enter the first row, use space or tabs between numbers "))
-        second = input(print("Enter the second row, use space or tabs between numbers"))
-        third = input(print("Enter the third row, use space or tabs between numbers "))
-        # enter your own puzzle code
+        first = input("Enter the first row, use space or tabs between numbers: ").split()
+        second = input("Enter the second row, use space or tabs between numbers: ").split()
+        third = input("Enter the third row, use space or tabs between numbers: ").split()
+        initial = first + second + third
+        initialState = [int(x) for x in initial]
+        problemVal = Problem(initialState,goalState)
     algChoice = input("Enter your choice of algorithm\n1. Uniform Cost Search\n2. A* with the Misplaced Tile heuristic.\n3. A* with the Euclidean distance heuristic.\n")
-    if algChoice == "1":
+    if algChoice.strip() == "1":
         print("uniform cost search code here")
-    elif algChoice == "2":
+        print(UniformCostSearch(problemVal))
+    elif algChoice.strip() == "2":
         print("A* with the Misplaced Tile heuristic code here")
-    elif algChoice == "3":
+    elif algChoice.strip() == "3":
         print("A* with the Euclidean distance heuristic. code here")
     else:
         print("invalid algorithm chosen")
