16a5611014852cf6150f1eba4c296b12b8624bb4
trisha112 <tagra002@ucr.edu>
2023-04-23 19:03:04 -0700

    Update UniformCostSearch.py

--- UniformCostSearch.py
+++ UniformCostSearch.py
@@ -2,25 +2,25 @@ from Problem import Problem
 from Node import Node
 #from Tree import Tree
 from queue import PriorityQueue
+
 def UniformCostSearch(problem):
-    print("in uniformCostSearch function")
     node = Node(problem.initial_state, 0)
-    q = PriorityQueue()
-    q.put(node)
-    explored = set()
-    while q.qsize != 0:
-        print("in while")
-        if q.empty():
-            print("failure")
+    q = []
+    q.append(node)
+    explored = []
+    while len(q) != 0:
+        if len(q) == 0:
             return "failure"
-        node = q.get()
+        node = q[0]
         if problem.goal_test(node.state):
-            print("problem.goal_test(node.state)")
             return node
-        explored.add(tuple(node.state))
-        print("explored.put(node.state)")
+        explored.append(node.state)
         for action in problem.actions(node.state):
-            child = child.node(problem,node,action)
-            print("forloop")
-            if (child.state not in explored) & (child.state not in q):
-                q.put(child,q)
+            child = node.child_node(problem,node,action)
+            if (child.state != explored) or (child.state != q):
+                print("if statement 1")
+                if problem.goal_test(child.state):
+                    print("if statement 2")
+                    return child
+                q.append(child)
+    return q
